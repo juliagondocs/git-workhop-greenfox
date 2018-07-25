9df4f39883bf6054c4e8f41d665feb110b1ed341
juliagondocs <julia.gondocs@gmail.com>
2018-07-25 20:57:02 +0200

    a6

--- future.java
+++ future.java
@@ -2,6 +2,7 @@ public class future {
 	public static void printHoroscopes (String yourSign){
 		System.out.println("Eszti: skorpio, Evelin: Ikrek", "Klari: taurus");
 		System.out.println(yourSign);
+		System.out.println("Your future is: " + yourSign);
 		}
     public static void main(String[] args) {
 	printHoroscopes();

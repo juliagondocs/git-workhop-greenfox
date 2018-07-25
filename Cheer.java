c1655dd67126a7a1a5eb047e769d784ee888b74b
juliagondocs <julia.gondocs@gmail.com>
2018-07-25 20:20:52 +0200

    aaaaa

--- Cheer.java
+++ Cheer.java
@@ -4,15 +4,17 @@ public class Main {
 	String valtozo;
 
     public static void main(String[] args) {
+	// write your code here
+        cheer();
 
+<<<<<<< HEAD
         String group = "Whitelabel Error";
         cheer(group);
+=======
+>>>>>>> b436415abb9d4019e9f3dfaf9e8870ba63b80a8e
     }
 
-    public static void cheer (String input) {
-
-
-        System.out.println(input);
+    public static void cheer () {
+        System.out.println("Java");
     }
-
 }
--- Greet.java
+++ Greet.java
@@ -1,6 +1,11 @@
 public class Greet {
+
+	public void greet () {
+		System.out.println("Greetings"); 
+
 	public void greet (String greeting) {
 		System.out.println(greeting + "Julcsika"); 
+
 	}
 }
 

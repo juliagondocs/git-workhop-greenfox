759273579a8574af0eec93cebc7aa47849bdccac
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-07-25 19:46:16 +0200

    d1

---
+++ future.java
@@ -0,0 +1,8 @@
+public class future {
+	public static void printHoroscopes (){
+		System.out.println("Eszti: skorpio, Evelin: Ikrek");}
+    public static void main(String[] args) {
+	printHoroscopes();
+
+    }
+}
